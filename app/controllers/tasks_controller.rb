c45bf845067188755320556a78ab14d8d95cbe23
Florante Dela Cruz <fndelacruz@gmail.com>
2016-05-22 11:53:55 -0700

    add task progress bar

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,13 @@ class TasksController < ApplicationController
   def index
     if current_user
       @tasks = Task.where(owner_id: current_user.id)
+      @tasks_done = @tasks.count{ |task| task.complete == 1 }
+      @tasks_left = @tasks.length - @tasks_done
+      if !@tasks.empty?
+        @tasks_pct = ((@tasks_done / @tasks.length.to_f) * 100).to_i
+      else
+        @tasks_pct = 0
+      end
       render :index
     else
       redirect_to '/session/new'
